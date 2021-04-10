b6847c498e6dff87cc07f7e3644a5c0a8d0d5002
Jimbo <marquardtjames@gmail.com>
2021-04-10 12:43:02 -0500

    more logic for grabbing page slugs

--- helpers/getPostSlugs.js
+++ helpers/getPostSlugs.js
@@ -1,5 +1,17 @@
-const glob = require('glob');
-glob("pages/**/*", (er, files) => {
-  console.log(files);
-});
+const glob = require("glob");
 
+const validExtensions = /\.(ts|js|md)x?$/;
+
+const hasValidExtension = (path) => path.match(validExtensions);
+const isntHidden = (path) => path && path[0] !== "_";
+const isntApi = (path) => path && path.slice(0,4) !== "api/"
+const isValidPath = (path) => {
+  return hasValidExtension(path) && isntHidden(path) && isntApi(path);
+};
+
+glob("pages/**/*", (er, files) => {
+  stripped = files.map((path) => path.slice(6));
+  matches = stripped.filter(isValidPath);
+  matches = matches.map(path=> path.replace(validExtensions, ""));
+  console.log(matches);
+});
