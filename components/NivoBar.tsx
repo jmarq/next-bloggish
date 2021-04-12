0ba2347945e2ded49b6da90dc8095f38707ea780
Jimbo <marquardtjames@gmail.com>
2021-04-12 00:08:49 -0500

    make NivoBar example component a bit more reusable. move example data into data dir. added another chart to nivoBar.mdx

--- components/NivoBar.tsx
+++ components/NivoBar.tsx
@@ -3,123 +3,17 @@
 import { ResponsiveBar } from "@nivo/bar";
 import { ThemeContext } from "styled-components";
 import { useContext } from "react";
-const data = [
-  {
-    country: "AD",
-    "hot dog": 58,
-    "hot dogColor": "hsl(308, 70%, 50%)",
-    burger: 148,
-    burgerColor: "hsl(69, 70%, 50%)",
-    sandwich: 145,
-    sandwichColor: "hsl(45, 70%, 50%)",
-    kebab: 197,
-    kebabColor: "hsl(127, 70%, 50%)",
-    fries: 60,
-    friesColor: "hsl(44, 70%, 50%)",
-    donut: 145,
-    donutColor: "hsl(49, 70%, 50%)",
-  },
-  {
-    country: "AE",
-    "hot dog": 16,
-    "hot dogColor": "hsl(18, 70%, 50%)",
-    burger: 27,
-    burgerColor: "hsl(157, 70%, 50%)",
-    sandwich: 135,
-    sandwichColor: "hsl(105, 70%, 50%)",
-    kebab: 124,
-    kebabColor: "hsl(294, 70%, 50%)",
-    fries: 56,
-    friesColor: "hsl(346, 70%, 50%)",
-    donut: 25,
-    donutColor: "hsl(333, 70%, 50%)",
-  },
-  {
-    country: "AF",
-    "hot dog": 58,
-    "hot dogColor": "hsl(149, 70%, 50%)",
-    burger: 91,
-    burgerColor: "hsl(348, 70%, 50%)",
-    sandwich: 167,
-    sandwichColor: "hsl(251, 70%, 50%)",
-    kebab: 147,
-    kebabColor: "hsl(184, 70%, 50%)",
-    fries: 111,
-    friesColor: "hsl(79, 70%, 50%)",
-    donut: 80,
-    donutColor: "hsl(356, 70%, 50%)",
-  },
-  {
-    country: "AG",
-    "hot dog": 21,
-    "hot dogColor": "hsl(74, 70%, 50%)",
-    burger: 193,
-    burgerColor: "hsl(311, 70%, 50%)",
-    sandwich: 155,
-    sandwichColor: "hsl(139, 70%, 50%)",
-    kebab: 76,
-    kebabColor: "hsl(176, 70%, 50%)",
-    fries: 122,
-    friesColor: "hsl(91, 70%, 50%)",
-    donut: 33,
-    donutColor: "hsl(300, 70%, 50%)",
-  },
-  {
-    country: "AI",
-    "hot dog": 115,
-    "hot dogColor": "hsl(295, 70%, 50%)",
-    burger: 16,
-    burgerColor: "hsl(185, 70%, 50%)",
-    sandwich: 10,
-    sandwichColor: "hsl(8, 70%, 50%)",
-    kebab: 160,
-    kebabColor: "hsl(357, 70%, 50%)",
-    fries: 23,
-    friesColor: "hsl(241, 70%, 50%)",
-    donut: 108,
-    donutColor: "hsl(150, 70%, 50%)",
-  },
-  {
-    country: "AL",
-    "hot dog": 132,
-    "hot dogColor": "hsl(135, 70%, 50%)",
-    burger: 10,
-    burgerColor: "hsl(222, 70%, 50%)",
-    sandwich: 179,
-    sandwichColor: "hsl(140, 70%, 50%)",
-    kebab: 138,
-    kebabColor: "hsl(193, 70%, 50%)",
-    fries: 36,
-    friesColor: "hsl(217, 70%, 50%)",
-    donut: 128,
-    donutColor: "hsl(261, 70%, 50%)",
-  },
-  {
-    country: "AM",
-    "hot dog": 164,
-    "hot dogColor": "hsl(150, 70%, 50%)",
-    burger: 125,
-    burgerColor: "hsl(26, 70%, 50%)",
-    sandwich: 42,
-    sandwichColor: "hsl(341, 70%, 50%)",
-    kebab: 39,
-    kebabColor: "hsl(45, 70%, 50%)",
-    fries: 173,
-    friesColor: "hsl(13, 70%, 50%)",
-    donut: 6,
-    donutColor: "hsl(146, 70%, 50%)",
-  },
-];
+import foodData from "data/food.json";
+
 // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
 // no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
-const NivoBar = (
-  {
-    /* see data tab */
-  }
-) => {
+const NivoBar = ({
+  data = foodData,
+  keys = ["hot dog", "burger", "sandwich", "kebab", "fries", "donut"],
+  indexBy = "country",
+  yLabel = "hot dog",
+}) => {
   const themeContext = useContext(ThemeContext);
   return (
     <ResponsiveBar
@@ -134,8 +28,8 @@ const NivoBar = (
         },
       }}
       data={data}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-      indexBy="country"
+      keys={keys}
+      indexBy={indexBy}
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
       valueScale={{ type: "linear" }}
@@ -161,20 +55,20 @@ const NivoBar = (
           spacing: 10,
         },
       ]}
-      fill={[
-        {
-          match: {
-            id: "fries",
-          },
-          id: "dots",
-        },
-        {
-          match: {
-            id: "sandwich",
-          },
-          id: "lines",
-        },
-      ]}
+      // fill={[
+      //   {
+      //     match: {
+      //       id: "fries",
+      //     },
+      //     id: "dots",
+      //   },
+      //   {
+      //     match: {
+      //       id: "sandwich",
+      //     },
+      //     id: "lines",
+      //   },
+      // ]}
       borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
       axisTop={null}
       axisRight={null}
@@ -182,7 +76,8 @@ const NivoBar = (
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "country",
+        // indexBy?
+        legend: indexBy,
         legendPosition: "middle",
         legendOffset: 32,
       }}
@@ -190,7 +85,8 @@ const NivoBar = (
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "food",
+        // keys[0] or ??
+        legend: yLabel,
         legendPosition: "middle",
         legendOffset: -40,
       }}
