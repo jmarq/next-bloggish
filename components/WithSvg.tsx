e42e086e6639e57c22c735edacf0e40cf466bc9c
Jimbo <marquardtjames@gmail.com>
2021-04-18 20:24:14 -0500

    pass props to wrapper div for WithSvg. more examples in post

--- components/WithSvg.tsx
+++ components/WithSvg.tsx
@@ -1,6 +1,6 @@
-const WithSvg = ({children}) => {
+const WithSvg = ({children, ...props}) => {
   return (
-    <div>
+    <div {...props}>
       <div>{children}</div>
       <svg height="1em" width="100%" viewBox="0 0 100 100"  style={{display: "block"}} version="1.1" preserveAspectRatio="none">
         <path stroke="black" fill="red" d="M 0,0 L 50, 100 L 100, 0 Z"></path>
