3e7cde670a068d9d1533a35c99a9dd6a2016e5ba
Jimbo <marquardtjames@gmail.com>
2021-04-19 20:15:04 -0500

    use theme colors in svg decoration

--- components/WithSvg.tsx
+++ components/WithSvg.tsx
@@ -1,10 +1,16 @@
+import { useContext} from "react";
+import { ThemeContext } from "styled-components";
+
+
 const WithSvg = ({children, ...props}) => {
+  const theme = useContext(ThemeContext);
   return (
     <div {...props}>
       <div>{children}</div>
       <svg height="1em" width="100%" viewBox="0 0 100 100"  style={{display: "block"}} version="1.1" preserveAspectRatio="none">
-        <path stroke="black" fill="red" d="M 0,0 L 50, 100 L 100, 0 Z"></path>
-        <rect x="0" y="0" width="50" height="50" fill="yellow"></rect>
+        <path stroke="none" fill={theme.colors.accent} d="M 0,0 L 50, 100 L 100, 0 Z"></path>
+        <path stroke="none" fill={theme.colors.primary} d="M 0,0 L 25, 100 L 50, 0 L 75, 100, L 100, 0 Z"></path>
+
       </svg>
     </div>
   );
