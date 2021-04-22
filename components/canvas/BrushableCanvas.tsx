53f2b58fba7da0f93aa8ba80151286f48cf2f662
Jimbo <marquardtjames@gmail.com>
2021-04-21 21:35:56 -0500

    elliptical brush with preview

--- components/canvas/BrushableCanvas.tsx
+++ components/canvas/BrushableCanvas.tsx
@@ -30,16 +30,36 @@ const BrushableCanvas = ({
           if (ev.mode == "drag") {
             const selectionWidth = ev.selection[1][0] - ev.selection[0][0];
             const selectionHeight = ev.selection[1][1] - ev.selection[0][1];
-            context.fillRect(
-              ev.selection[0][0],
-              ev.selection[0][1],
-              selectionWidth,
-              selectionHeight
+            const midpoint = [
+              (ev.selection[1][0] + ev.selection[0][0]) / 2,
+              (ev.selection[1][1] + ev.selection[0][1]) / 2,
+            ];
+            context.beginPath();
+            context.ellipse(
+              midpoint[0],
+              midpoint[1],
+              selectionWidth / 2,
+              selectionHeight / 2,
+              0,
+              0,
+              2 * Math.PI
             );
+            context.fill();
+            // context.ellipse(0,0,10,10,0,0,Math.PI*2);
+
+            // context.fillRect(
+            //   ev.selection[0][0],
+            //   ev.selection[0][1],
+            //   selectionWidth,
+            //   selectionHeight
+            // );
           }
         });
       const selectedBrush = d3Select(brushElement as SVGGElement);
       brush(selectedBrush);
+      const brushSelection = brushElement.querySelector("rect.selection");
+      brushSelection.setAttribute("fill", "url(#brush-preview)");
+      brushSelection.setAttribute("fill-opacity", "0.7");
     }
   }, [canvas.current, canvasBrush.current]);
   return (
@@ -64,6 +84,18 @@ const BrushableCanvas = ({
           boxSizing: "content-box",
         }}
       >
+        <defs>
+          <pattern
+            id="brush-preview"
+            patternUnits="objectBoundingBox"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="none"
+            viewBox="0 0 100 100"
+          >
+            <circle cx="50" cy="50" r="50" fill={paintColor}></circle>
+          </pattern>
+        </defs>
         <g ref={canvasBrush}></g>
       </svg>
       {colorPicker && (
