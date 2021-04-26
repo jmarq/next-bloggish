5f8d2ddbf23fa89247d5f5e5c7246fe073b5225a
Jimbo <marquardtjames@gmail.com>
2021-04-25 20:51:20 -0500

    working on getting arrests into bar chart

--- components/urbana/Arrests.tsx
+++ components/urbana/Arrests.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import NivoBar from "components/NivoBar";
 
 const myClient = new QueryClient();
 
-
 const getData = async () => {
-  const response = await fetch("https://data.urbanaillinois.us/resource/afbd-8beq.json");
+  const response = await fetch(
+    "https://data.urbanaillinois.us/resource/afbd-8beq.json"
+  );
   const json = await response.json();
   // const result = await response.json();
-  return json;
-}
+  return json.map((el) => ({
+    age_at_arrest: Number(el.age_at_arrest),
+    year_of_arrest: el.year_of_arrest,
+  }));
+};
 
 const DataViewer = () => {
   const { data, isLoading, error } = useQuery("arrests", getData);
   return (
     <div>
+      {data && (
+        <div
+          style={{ height: 500 }}
+        >
+          <NivoBar
+            data={data}
+            keys={["age_at_arrest"]}
+            indexBy={"year_of_arrest"}
+          />
+        </div>
+      )}
       {JSON.stringify(data)}
     </div>
   );
@@ -26,6 +42,6 @@ const ArrestQuery = () => {
       <DataViewer></DataViewer>
     </QueryClientProvider>
   );
-}
+};
 
 export default ArrestQuery;
