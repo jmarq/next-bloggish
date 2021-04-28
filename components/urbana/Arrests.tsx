686462cb0e20e8f29bb48eef39dfefc64261d5f2
Jimbo <marquardtjames@gmail.com>
2021-04-27 22:06:50 -0500

    data wrangling adventures with arquero

--- components/urbana/Arrests.tsx
+++ components/urbana/Arrests.tsx
@@ -1,44 +1,75 @@
 import React from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import _ from "lodash";
+import { table, from, op } from "arquero";
 import NivoBar from "components/NivoBar";
 
 const myClient = new QueryClient();
-
 const getData = async () => {
   const response = await fetch(
     "https://data.urbanaillinois.us/resource/afbd-8beq.json"
   );
   const json = await response.json();
-  // const result = await response.json();
-  // return json.map((el) => ({
-  //   age_at_arrest: Number(el.age_at_arrest),
-  //   year_of_arrest: el.year_of_arrest,
-  //   arrest_code: el.arrest_code
-  // }));
   return json;
 };
 
+// this got kind of nasty. need more practice with cleanly doing nested rollups in Arquero
 const prepareData = (data) => {
-  const grouped = _.groupBy(data, "arrestee_sex");
-  const results = Object.entries(grouped).map((el) => ({
-    race: el[0],
-    count: el[1].length,
-  }));
-  return results;
+  let results = [];
+  let crimes = [];
+  if (data) {
+    let table = from(data);
+    // sigh
+    table = table.rename({ arrestee_sex: "race", arrestee_race: "sex" });
+    crimes = data.map((d) => d.crime_category_description);
+    crimes = Array.from(new Set(crimes));
+    console.log(crimes);
+
+    results = table
+      .groupby({
+        race: (d: any) => d.race,
+      })
+      // .pivot("crime_category_description", { hmm: () => 1 })
+      // .pivot({ key: (d) => "hey" }, { value: (d) => op.sum(d) })
+      // // .count()
+      .objects({ grouped: "entries" });
+
+    let finalResults = [];
+    for (let entry of results) {
+      const [race, list] = entry;
+      let entryTable = from(list);
+      let grouped = entryTable
+        .groupby("crime_category_description")
+        .count()
+        .pivot("crime_category_description", "count")
+        .objects({ grouped: "object" });
+      let entryResults = {
+        race,
+        ...grouped[0],
+      };
+      finalResults.push(entryResults);
+    }
+    results = finalResults;
+  }
+  results = Array.from(results);
+  // return results;
+  return {
+    preparedData: results,
+    keys: crimes,
+  };
 };
 
 const DataViewer = () => {
   const { data, isLoading, error } = useQuery("arrests", getData);
   console.log(prepareData(data));
-  const preparedData = prepareData(data);
+  const { preparedData, keys } = prepareData(data);
   return (
     <div>
       {data && (
         <div style={{ height: 500 }}>
           <NivoBar
             data={preparedData}
-            keys={["count"]}
+            keys={keys}
             indexBy={"race"}
             yLabel="count"
           />
