ce86fb65115b5990079ba50257e6737b83eac12a
Jimbo <marquardtjames@gmail.com>
2021-05-18 21:55:55 -0500

    comments about webpack/dependency issues

--- components/urbana/Arrests.tsx
+++ components/urbana/Arrests.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-import { from } from "arquero";
+import {from} from "arquero";
 import { ResponsiveCalendar } from "@nivo/calendar";
 import NivoBar from "components/charts/NivoBar";
 import NivoCalendar from "components/charts/NivoCalendar";
--- next.config.js
+++ next.config.js
@@ -4,5 +4,8 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   exportTrailingSlash: true,
+  // webpack 5 seems to break arquero, because of an import/export issue
+  // webpack 4 seems to choke on ?? operator from vega. how to correctly use babel loaders for node modules in next?
+  // stalemate for now...
   future: { webpack5: true }
 })
