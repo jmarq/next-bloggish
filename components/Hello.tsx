e306e3955244dde30c6a342204a680c16c0a5652
Jimbo <marquardtjames@gmail.com>
2021-05-20 22:11:46 -0500

    use theme values to populate var()s in stylesheets, experimenting

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -1,7 +1,17 @@
 import styles from "styles/hello.module.scss";
-
+import styled from 'styled-components'
+import { theme1 } from "theme";
 // uses scss modules, unlike most of the other components that use styled components, styled system, etc.
 // note the .module.scss suffix on the file. note the styles[] syntax.
+
+// this is probably silly but I'm experimenting. using theme to set custom property that is referred to by scss module.
+// perhaps a way to inject theme styles into components that heavily use stylesheets.
+// could maybe replace "hardcoded values" with var(--a-variable, the-previously-hardcoded-value-as-fallback)
+// the complexity may not be worth it, though.
+export const Wrapper = styled.div`
+--theme-accent-color: ${({theme}) => theme.colors.accent};
+`
+
 const Hello = ({}) => {
   return (
     <div className={styles['hello']}>Hello</div>
